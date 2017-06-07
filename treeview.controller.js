4262ecc57df362e136fba793ad06217e2d88391e
Sagar Solanki <sagar.solanki@outlook.com>
2017-06-07 18:05:58 +0530

    Add files via upload

---
+++ treeview.controller.js
@@ -0,0 +1,92 @@
+'use strict';
+
+var app = angular.module("KendoDemos", ["kendo.directives"]);
+
+var jsondata = [{
+        "id": 1,
+        "text": "Furniture",
+        "parentId": null
+    },
+    {
+        "id": 2,
+        "text": "Tables & Chairs",
+        "parentId": null
+    },
+    {
+        "id": 3,
+        "text": "something",
+        "parentId": 1
+    },
+    {
+        "id": 4,
+        "text": "Sofas",
+        "parentId": null
+    },
+    {
+        "id": 5,
+        "text": "Occasional Furniture",
+        "parentId": 2
+    },
+    {
+        "id": 6,
+        "text": "Decor",
+        "parentId": null
+    },
+    {
+        "id": 7,
+        "text": "Bed Linen",
+        "parentId": null
+    },
+    {
+        "id": 8,
+        "text": "Curtains & Blinds",
+        "parentId": 1
+    },
+    {
+        "id": 9,
+        "text": "Carpets",
+        "parentId": null
+    }
+];
+
+app.controller("MyCtrl", function ($scope) {
+
+       $scope.traverseTree = function (jsondata) {
+        var map = {},
+            node, roots = [];
+        for (var i = 0; i < jsondata.length; i += 1) {
+            node = jsondata[i];
+            node.items = [];
+            node.hasChildren = false;
+            map[node.id] = i; // use map to look-up the parents
+            if (node.parentId !== null) {
+                jsondata[map[node.parentId]].items.push(node);
+                jsondata[map[node.parentId]].hasChildren = true;
+            } else {
+                roots.push(node);
+            }
+        }
+        $scope.things = roots;
+        console.log(roots);
+    };
+
+    $scope.traverseTree(jsondata);
+
+    var dataSource = new kendo.data.HierarchicalDataSource({
+        data: {
+            things
+        },
+        schema: {
+            model: {
+                id: "id",
+                hasChildren: "hasChildren"
+            }
+        },
+        dataTextField: "text"
+    });
+
+    $scope.thingsOptions = {
+        dataSource: things
+    };
+
+});
